9b8898946ca0cafc958bf6062d8cb5ca771e6a40
shovelfishy <edwardmyliew@gmail.com>
2024-10-20 00:25:19 -0700

    display

--- src/components/transcript.tsx
+++ src/components/transcript.tsx
@@ -7,7 +7,7 @@ const SubtitlesDisplay: React.FC = () => {
   const subtitles = useSubtitlesStore((state) => state.subtitles);
 
   return (
-    <div className="p-2 pt-20 top-0 w-96 bg-white text-black overflow-y-auto h-screen">
+    <div className="p-2 pt-16 w-[400px] shrink-0 top-0 bg-white text-black overflow-y-auto h-screen">
       {subtitles.map((subtitle, index) => (
         <div key={index}>
           <strong>{subtitle.username}:</strong> {subtitle.text}
